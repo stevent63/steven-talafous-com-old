384a29615fd681a215a010149cd781bee37b752c
Steven Talafous <steven.talafous@gmail.com>
2023-01-12 17:53:26 -0500

    https support added

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
+# st-registry.tenant-5c610a-dev.lga1.ingress.coreweave.cloud/steven-talafous-com:1
+
 FROM node:18
 
 WORKDIR /app
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+import https from 'https'
 import express from 'express'
 const app = express();
+
+const options = {};
 
 app.get('/', (req, res) => {
   res.redirect('https://twitter.com/steventlive');
 });
 
+const server = https.createServer(options, app);
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Serving on port ${port}`);
 });
